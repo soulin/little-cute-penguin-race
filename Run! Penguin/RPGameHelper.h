de9c5ba45c29522ca715eae79e70ca6bbbb3edf0
Sean <Sean@Seans-iMac.local>
2013-04-16 12:23:05 +0800

    push test

--- Run! Penguin/RPGameHelper.h
+++ Run! Penguin/RPGameHelper.h
@@ -7,7 +7,7 @@
 //
 
 #ifndef Run__Penguin_RPGameHelper_h
-
+////////////////////////////////////////////////////////////////////////////
 #define Run__Penguin_RPGameHelper_h
 #define SELECTOR_STRING [NSString stringWithFormat:@"[%@ %@]: ",NSStringFromClass([self class]), NSStringFromSelector(_cmd)]
 #define RPGameCenterLocalPlayerAuthenticationChanged @"RPGameCenterLocalPlayerAuthenticationChanged"
@@ -16,4 +16,5 @@
 #define RPNewSceneHasBecomeCurrentScene @"RPNewSceneHasBecomeCurrentScene"
 #define RPCustomerGainedGemsCountDidChanged @"RPCustomerGainedGemsCountDidChanged"
 #define RPCustomerGainedGoodsCountDidChanged @"RPCustomerGainedGoodsCountDidChanged"
+////////////////////////////////////////////////////////////////////////////
 #endif
